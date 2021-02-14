dd72f762a1231d2af83b20cc88af86083216402b
Pierre V <746601+pierre-vo@users.noreply.github.com>
2021-02-14 23:49:51 +0100

    Pull ups on i2c until the 2nd half is build

--- keyboards/handwired/dactyl_manuform/5x7/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define PRODUCT         Dactyl-Manuform (5x7)
 
-#define DEBUG_MATRIX_SCAN_RATE
+//#define DEBUG_MATRIX_SCAN_RATE
 
 /* key matrix size */
 // Rows are doubled-up
