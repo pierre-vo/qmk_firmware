269d27e7469b418aae8bd7fc02845b9edb516edb
Pierre V <746601+pierre-vo@users.noreply.github.com>
2021-06-05 16:23:24 +0200

    dactyl manuform: update keymap

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/torreip/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/torreip/keymap.c
@@ -6,6 +6,11 @@
 extern keymap_config_t keymap_config;
 
 
+/* Fillers to make layering more clear */
+//#define _______ KC_TRNS
+#define ___T___ KC_TRNS
+#define XXXXXXX KC_NO
+
 /* Layer shorthand */
 enum layers {
   _QWERTY = 0, /* default layer */
@@ -43,54 +48,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LGUI,   KC_LALT,
                                KC_SPC,   KC_BSPC,
                                KC_LCTL,  LOWER,
-                               RAISE,    KC_HOME,
+                               KC_LALT,  KC_HOME,
         // right hand
                      RESET,   KC_6,    KC_7,    KC_8,     KC_9,     KC_0,     KC_MINS,
                      KC_RBRC, KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_EQL,
                      KC_MEH,  KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-                              KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS,
-                                                ALTSLSH,  KC_SLSH,
+                              KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_RGHT,  KC_BSLS,
+                                                ALTSLSH,  KC_LEFT,
         KC_ENT,  RAISE,
-        KC_RSFT, LOWER,
-        KC_PGUP, KC_PGDN),
+        KC_DEL,  KC_RSFT,
+        KC_UP,   KC_DOWN),
 
 [_LOWER] = LAYOUT_5x7(
   // left hand
-   _______,   KC_F1,     KC_F2,      KC_F3,    KC_F4,     KC_F5,    KC_F6,
-   _______,   _______,   _______,    KC_UP,    _______,   _______,  _______,
-   _______,   _______,   KC_LEFT,    KC_DOWN,  KC_RGHT,   _______,  RESET,
-   _______,   _______,   _______,   _______,   _______,   _______,
+   _______,   KC_F1,     KC_F2,     KC_F3,    KC_F4,     KC_F5,    KC_F6,
+   _______,   _______,   _______,   KC_UP,    _______,   _______,  _______,
+   _______,   _______,   KC_LEFT,   KC_DOWN,  KC_RGHT,   _______,  RESET,
+   _______,   _______,   _______,   _______,  _______,   _______,
                          KC_MPRV,   KC_MNXT,
                                _______, _______,
-                               _______, _______,
+                               _______, ___T___,
                                _______, _______,
         // right hand
-                     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,
-                     _______,   _______,   _______,   _______,   _______,   _______,   _______,
-                     _______,   _______,   KC_L,      _______,   _______,   _______,   _______,
-                                _______,   _______,   _______,   _______,   _______,   _______,
-                                                      _______,   _______,
-        KC_DEL, _______,
+                     KC_F7,     KC_F8,     KC_F9,     KC_F10,  KC_F11,  KC_F12,    _______,
+                     _______,   _______,   KC_P7,     KC_P8,   KC_P9,   KC_ASTR,   _______,
+                     KC_NLCK,   _______,   KC_P4,     KC_P5,   KC_P6,   KC_PLUS,   _______,
+                                _______,   KC_P1,     KC_P2,   KC_P3,   KC_BSLS,   _______,
+                                                      KC_P0,   KC_PDOT,
+        KC_DEL,  _______,
         _______, _______,
-        _______, _______),
+        KC_LEFT, KC_RGHT),
 
 [_RAISE] = LAYOUT_5x7(
   // left hand
    _______,   _______,   _______,   _______,   _______,   _______,  _______,
-   _______,   _______,   _______,   _______,   _______,   _______,  _______,
-   _______,   _______,   _______,   _______,   _______,   _______,  _______,
+   _______,   _______,   _______,   KC_UP,     _______,   _______,  _______,
+   _______,   _______,   KC_LEFT,   KC_DOWN,   KC_RGHT,   _______,  _______,
    _______,   _______,   _______,   _______,   _______,   _______,
                          _______,   _______,
                                _______, _______,
                                _______, _______,
                                _______, _______,
         // right hand
-                     _______,   _______,   KC_NLCK,   _______,   KC_PMNS,   KC_PPLS,   _______,
-                     _______,   _______,   KC_P7,     KC_P8,     KC_P9,     _______,   _______,
-                     _______,   _______,   KC_P4,     KC_P5,     KC_P6,     KC_PAST,   _______,
-                                _______,   KC_P1,     KC_P2,     KC_P3,     KC_PSLS,   _______,
-                                                      KC_P0,     KC_PDOT,
-        _______, _______,
+                     _______,   _______,   _______,   _______,   _______,   _______,   _______,
+                     _______,   _______,   _______,   _______,   _______,   _______,   _______,
+                     RESET,     _______,   _______,   _______,   _______,   _______,   _______,
+                                _______,   KC_VOLD,   KC_VOLU,   KC_MUTE,   _______,   _______,
+                                                      _______,   _______,
+        _______, ___T___,
         _______, _______,
         _______, _______),
 
